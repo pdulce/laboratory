94a8086a8ef137b86a23d866f4343ebb5bd59f78
pedro.dulce <pedro.dulce@gmail.com>
2023-11-08 23:35:12 +0100

    mejoras+

--- src/main/java/com/mylabs/pds/service/GitHubViaApiRest.java
+++ src/main/java/com/mylabs/pds/service/GitHubViaApiRest.java
@@ -3,6 +3,7 @@ package com.mylabs.pds.service;
 import com.mylabs.pds.model.Tarea;
 import com.mylabs.pds.repository.ConfiguracionRepository;
 import com.mylabs.pds.repository.TareaRepository;
+import com.mylabs.pds.utils.GitHubContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -23,7 +24,6 @@ public class GitHubViaApiRest {
     private static final String GITHUB_API_URL = "https://api.github.com";
     private RestTemplate restTemplate;
     private byte[] bytesOfZipped;
-    private List<Tarea> tareas;
 
     @Autowired
     private TareaRepository tareaRepository;
@@ -35,27 +35,57 @@ public class GitHubViaApiRest {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    public List<Tarea> scanRepository(String owner, String repo) {
+    public List<Tarea> scanRepository(String owner, String repository) {
         String token = this.configRepository.findById(1L).isPresent()
                 ? this.configRepository.findById(1L).get().getCodigo() : "unknown";
-        String url2 = GITHUB_API_URL + "/repos/" + owner + "/" + repo;
 
-        String url = GITHUB_API_URL + "/repos/" + owner + "/" + repo + "/contents?ref=develop";
-
+        String baseUriPattern = "https://api.github.com/repos/%s/%s/contents%s?ref=%s";
+        String initDirBase = "/src/main/java";
+        String branch = "develop";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.set("Authorization", "token " + token);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        this.tareas = new ArrayList<>();
+        String newGithubApiUrl = String.format(baseUriPattern, owner, repository, initDirBase , branch);
+        ResponseEntity<GitHubContent[]> responseInner = restTemplate.exchange(newGithubApiUrl,
+                HttpMethod.GET, entity, GitHubContent[].class);
+        GitHubContent[] contentsInner = responseInner.getBody();
+        for (GitHubContent contentItem : contentsInner) {
+            // llamada recursiva para descubrir los fuentes java
+            scanDir(contentItem, baseUriPattern, owner, repository, initDirBase, branch, entity);
+        }
 
-        ResponseEntity<String> response =  restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-
-        // Procesa la respuesta
-        String responseBody = response.getBody();
-
-        System.out.println("responseBody: " + responseBody);
-
-        return this.tareas;
+        List<Tarea> tareas = new ArrayList<>();
+        //meter logica que ya tienes hecha en otras clases
+        return tareas;
     }
+
+    private void scanDir(final GitHubContent content, final String baseUriPattern,
+                         final String owner, final String repository,
+            final String initDirBase, final String branch, final HttpEntity<String> entity) {
+
+        if (content.getType().equals("file") && content.getName().endsWith(".java")) {
+            // El archivo es un archivo Java, lee su contenido
+            String fileContentUrl = content.getDownload_url();
+            ResponseEntity<String> fileResponse = restTemplate.exchange(fileContentUrl,
+                    HttpMethod.GET, entity, String.class);
+            String javaFileContent = fileResponse.getBody();
+            System.out.println("Contenido del archivo " + content.getName() + ":\n" + javaFileContent);
+        } else {
+            RestTemplate restTemplate = new RestTemplate();
+            System.out.println("el archivo "+  content.getName() + " es un " + content.getType()
+                    + "...lanzo una llamada recursiva");
+            String newGithubApiUrl = String.format(baseUriPattern, owner, repository,
+                    initDirBase + "/" + content.getName(), branch);
+            ResponseEntity<GitHubContent[]> responseInner = restTemplate.exchange(newGithubApiUrl,
+                    HttpMethod.GET, entity, GitHubContent[].class);
+            GitHubContent[] contentsInner = responseInner.getBody();
+            for (GitHubContent contentItem : contentsInner) {
+                scanDir(contentItem, baseUriPattern, owner, repository,
+                        initDirBase  + "/" + content.getName(), branch, entity);
+            }
+        }
+    }
+
 }
---
+++ src/main/java/com/mylabs/pds/utils/GitHubContent.java
@@ -0,0 +1,33 @@
+package com.mylabs.pds.utils;
+
+public class GitHubContent {
+    private String type;
+    private String name;
+    private String download_url;
+
+    // Agrega los getter y setter de las propiedades
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDownload_url() {
+        return download_url;
+    }
+
+    public void setDownload_url(String download_url) {
+        this.download_url = download_url;
+    }
+}
