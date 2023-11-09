23904de01df6b251d10d1d70b753e854f041810b
pedro.dulce <pedro.dulce@gmail.com>
2023-11-09 13:59:35 +0100

    mejoras+

--- src/main/java/com/mylabs/pds/service/GitHubApiRestAccessService.java
+++ src/main/java/com/mylabs/pds/service/GitHubApiRestAccessService.java
@@ -3,7 +3,9 @@ package com.mylabs.pds.service;
 import com.mylabs.pds.model.Tarea;
 import com.mylabs.pds.repository.ConfiguracionRepository;
 import com.mylabs.pds.repository.TareaRepository;
-import com.mylabs.pds.utils.*;
+import com.mylabs.pds.utils.GitHubContent;
+import com.mylabs.pds.utils.IClassGenerator;
+import com.mylabs.pds.utils.ZipUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -22,7 +24,11 @@ import java.util.List;
 public class GitHubApiRestAccessService {
 
     private static final String GITHUB_API_URL = "https://api.github.com";
+
+    private static final String GITLAB_API_URL = "https://giss.gitlab.repo...elque sea";
     private static final String INIT_BASE_DIR = "/src/main/java";
+
+    private static final String BRANCH_NAME = "develop";
     private RestTemplate restTemplate;
 
     @Autowired
@@ -37,28 +43,49 @@ public class GitHubApiRestAccessService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
+    private String accessGitLab(final String owner, final String repository, final HttpHeaders headers) {
+
+        String token = this.configRepository.findById(2L).isPresent()
+                ? this.configRepository.findById(2L).get().getCodigo() : "unknown";
+        String baseUriPattern = GITLAB_API_URL + "/projects/%s/%s/repository/tree?ref=%s";
+        headers.set("PRIVATE-TOKEN", token); // Use "PRIVATE-TOKEN" header for GitLab
+        return String.format(baseUriPattern, owner, repository, INIT_BASE_DIR, BRANCH_NAME);
+    }
+
+    private String accessGitHub(final String owner, final String repository, final HttpHeaders headers) {
+
+        String token = this.configRepository.findById(1L).isPresent()
+                ? this.configRepository.findById(1L).get().getCodigo() : "unknown";
+        String baseUriPattern = GITHUB_API_URL + "/repos/%s/%s/contents%s?ref=%s";
+        headers.set("Authorization", "token " + token);
+        return String.format(baseUriPattern, owner, repository, INIT_BASE_DIR , BRANCH_NAME);
+    }
+
     public List<Tarea> scanRepository(final String owner, final String repository,
                                       final IClassGenerator classGenerator) {
+
         this.classGenerator = classGenerator;
         String token = this.configRepository.findById(1L).isPresent()
                 ? this.configRepository.findById(1L).get().getCodigo() : "unknown";
 
-        String baseUriPattern = GITHUB_API_URL + "/repos/%s/%s/contents%s?ref=%s";
-        String initDirBase = INIT_BASE_DIR;
-        String branch = "develop";
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("Authorization", "token " + token);
+
+        String newGitApiUrl = accessGitHub(owner, repository, headers);
+        //String newGitApiUrl = accessGitLab(owner, repository, headers);
+
+        String baseUriPattern = GITHUB_API_URL + "/repos/%s/%s/contents%s?ref=%s";
+        //String baseUriPattern = GITLAB_API_URL + "/repos/%s/%s/contents%s?ref=%s";
+
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        String newGithubApiUrl = String.format(baseUriPattern, owner, repository, initDirBase , branch);
-        ResponseEntity<GitHubContent[]> responseInner = restTemplate.exchange(newGithubApiUrl,
-                HttpMethod.GET, entity, GitHubContent[].class);
+        ResponseEntity<GitHubContent[]> responseInner = restTemplate.exchange(newGitApiUrl, HttpMethod.GET, entity,
+                GitHubContent[].class);
         GitHubContent[] contentsInner = responseInner.getBody();
         List<Tarea> tareas = new ArrayList<>();
         for (GitHubContent contentItem : contentsInner) {
             // llamada recursiva para descubrir los fuentes java
-            tareas.add(scanDir(contentItem, baseUriPattern, owner, repository, initDirBase, branch, entity));
+            tareas.add(scanDir(contentItem, baseUriPattern, owner, repository, INIT_BASE_DIR, BRANCH_NAME, entity));
         }
         byte[] bytesOfZipped = new ZipUtil().generarZipDesdeTareas(tareas);
         tareas = this.tareaRepository.saveAll(tareas);
